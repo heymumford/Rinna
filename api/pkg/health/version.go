3fd0d1c34fb7a23d170e8472725a6082253044f2
emumford <eric.mumford@example.com>
2025-04-05 23:21:35 -0400

    Fix Go version mismatch in api/pkg/health/version.go

--- api/pkg/health/version.go
+++ api/pkg/health/version.go
@@ -10,7 +10,7 @@ package health
 
 // These values are set during the build process
 var (
-	Version   = "1.3.6"
+	Version   = "1.3.7"
 	CommitSHA = "runtime"
 	BuildTime = "2025-04-06T03:21:25Z"
 )
