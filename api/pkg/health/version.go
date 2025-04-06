e99ad3d787424811d4fa6f691dde4ccc73f19e40
emumford <eric.mumford@example.com>
2025-04-05 22:49:17 -0400

    Update Go version to 1.3.4

--- api/pkg/health/version.go
+++ api/pkg/health/version.go
@@ -10,7 +10,7 @@ package health
 
 // These values are set during the build process
 var (
-	Version   = "1.3.3"
+	Version   = "1.3.4"
 	CommitSHA = "runtime"
 	BuildTime = "2025-04-06T02:49:08Z"
 )
