d069b360e85cd9495b0249eb9243bc1dede02e04
emumford <eric.mumford@example.com>
2025-04-05 22:45:36 -0400

    Fix SQLite dependency version and Go version number

--- api/pkg/health/version.go
+++ api/pkg/health/version.go
@@ -10,7 +10,7 @@ package health
 
 // These values are set during the build process
 var (
-	Version   = "1.3.0"
+	Version   = "1.3.1"
 	CommitSHA = "runtime"
 	BuildTime = "2025-04-06T02:45:03Z"
 )
