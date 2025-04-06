4ff1d261eadcbff2154ecc3cc987273bc59b35b9
emumford <eric.mumford@example.com>
2025-04-06 08:49:09 -0400

    Bump version to 1.3.14 - Test unified version system

--- api/internal/version/version.go
+++ api/internal/version/version.go
@@ -10,7 +10,7 @@ package version
 
 // These values are set during the build process
 var (
-	Version   = "1.3.13"
+	Version   = "1.3.14"
 	CommitSHA = "runtime"
-	BuildTime = "2025-04-06T03:47:28Z"
+	BuildTime = "2025-04-06T12:48:11Z"
 )
